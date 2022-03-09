ed5cda19a9b88d53584ac159b56dbf1e58318852
joannesanthosh <99355156+joannesanthosh@users.noreply.github.com>
2022-03-09 17:37:06 +0000

    add script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Joanne Santhosh All rights reserved
+//
+// Created by: Joanne Santhosh
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+}
